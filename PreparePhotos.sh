1678ad088c41543ea3a4b3d319e1d9aa747b9058
Gitdev42 <marcel_bode@web.de>
2019-03-17 01:51:51 +0100

    Added automated compression and renamed Sample Pictures

--- PreparePhotos.sh
+++ PreparePhotos.sh
@@ -6,20 +6,18 @@ Watermark="Watermark_Test/Watermark/Watermark.png"
 declare -a PhotoFileExtension=("JPG" "jpg" "png")
 
 rm -rf ${PhotoOutputDir}
-echo "abc ${PhotoOutputDir}"
 mkdir ${PhotoOutputDir}
 
-for PhotoFileExtensions in ${PhotoFileExtension}; do
+for PhotoFileExtensions in ${PhotoFileExtension[@]}; do
   for FileName in ${PhotoInputDir}/*.${PhotoFileExtensions}; do
     FileWithExtension="$(basename -- ${FileName})"
     File=${FileWithExtension%.*}
-    echo "${PhotoOutputDir}/${File}_WM.JPG"
     composite -gravity SouthWest -geometry +10+10 ${Watermark} ${PhotoInputDir}/${FileWithExtension} ${PhotoOutputDir}/${File}_WM.JPG
-    echo "${File}"
   done;
 done;
 
 for FileName in ${PhotoOutputDir}/*.JPG; do
-  File="$(basename -- ${FileName})"
-  convert ${i} -quality 50% ${PhotoOutputDir}/${File}_50p.JPG
+  FileWithExtension="$(basename -- ${FileName})"
+  File=${FileWithExtension%.*}
+  convert ${FileName} -quality 50% ${PhotoOutputDir}/${File}_50p.JPG
 done;
