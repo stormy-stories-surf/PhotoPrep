2c1cce368315df575000fd4fc856a7f4f73aa300
Gitdev42 <>
2019-07-21 12:53:12 +0200

    added option to harmonize image sizes

--- PreparePhotos.sh
+++ PreparePhotos.sh
@@ -7,7 +7,8 @@ POST_CATEGORY="Watermark_Test"
 POST_NAME="WatermarkedPhotos"
 WATERMARK="Watermark_Test/Watermark/Watermark.png"
 SIZED_WATERMARK="Watermark_Test/Watermark/SIZED_WATERMARK.png"
-COMPRESSION_FACTOR="70"
+COMPRESSION_FACTOR="80"
+HARMONIZE_IMAGESIZES="true"
 
 # create output directory
 PHOTO_OUTPUT_DIR="${POST_CATEGORY}/${POST_NAME}"
@@ -21,34 +22,48 @@ for FILE_NAME in ${PHOTO_INPUT_DIR}/*.*; do
     FILE=${FILE_WITH_EXTENSION%.*}
 
     OUTPUT_FILE_NAME="${FILE}.jpg"
-    OUTPUT_FILE_NAME_COMPRESSED="${FILE}.jpg"
 
     # check if the converted image already exists
-    if ! [ -f "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME_COMPRESSED}" ]; then
+    if ! [ -f "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}" ]; then
 
-      # calculate a vector to resize the watermark depending in the image size and finaly resize the watermark
-      VARIABLE_HEIGHT=$(identify -ping -format '%h' ${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION} )
-      VARIABLE_WIDTH=$(identify -ping -format '%w' ${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION} )
-      #ImageSize=$((${VARIABLE_HEIGHT}*${VARIABLE_WIDTH})) ?? Version 0.3.0  calculate a vector for panorama pictures
-      #Vector="$(bc -l <<< "($ImageSize / 20256000)*100")%"
+      # harmonize all images sizes
+      if [ ${HARMONIZE_IMAGESIZES} == "true" ]; then
+        convert "${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION}" -resize 2666666@ "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}"
 
-      # check if resize is nessesary
-      if ! [ ${REMEMBERED_iMAGE_SIZE} == ${VARIABLE_WIDTH} ]; then
-        VECTOR="$(bc -l <<< "($VARIABLE_WIDTH / 6000)*100")%"
-        convert ${WATERMARK} -resize ${VECTOR} ${SIZED_WATERMARK}
-        REMEMBERED_iMAGE_SIZE=${VARIABLE_WIDTH}
+        # calculate a vector to resize the watermark depending in the image size and finaly resize the watermark
+        VARIABLE_HEIGHT=$(identify -ping -format '%h' ${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME} )
+        VARIABLE_WIDTH=$(identify -ping -format '%w' ${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME} )
+      else
+
+        # calculate a vector to resize the watermark depending in the image size and finaly resize the watermark
+        VARIABLE_HEIGHT=$(identify -ping -format '%h' ${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION} )
+        VARIABLE_WIDTH=$(identify -ping -format '%w' ${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION} )
+        #ImageSize=$((${VARIABLE_HEIGHT}*${VARIABLE_WIDTH})) ?? Version 0.3.0  calculate a vector for panorama pictures
+        #Vector="$(bc -l <<< "($ImageSize / 20256000)*100")%"
       fi
 
-      # watermark the image
-      composite -gravity SouthWest -geometry +10+10   ${SIZED_WATERMARK} ${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION} "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}"
+      # check if resize is nessesary
+      if ! [ ${REMEMBERED_IMAGE_SIZE} == ${VARIABLE_WIDTH} ]; then
+        VECTOR="$(bc -l <<< "($VARIABLE_WIDTH / 6000)*100")%"
+        convert ${WATERMARK} -resize ${VECTOR} ${SIZED_WATERMARK}
+        REMEMBERED_IMAGE_SIZE=${VARIABLE_WIDTH}
+      fi
+
+      if [ ${HARMONIZE_IMAGESIZES} == "true" ]; then
+        # watermark the image
+        composite -gravity SouthWest -geometry +10+10   ${SIZED_WATERMARK} ${PHOTO_OUTPUT_DIR}/${FILE_WITH_EXTENSION} "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}"
+      else
+        composite -gravity SouthWest -geometry +10+10   ${SIZED_WATERMARK} ${PHOTO_INPUT_DIR}/${FILE_WITH_EXTENSION} "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}"
+      fi
+
 
       # erase the metadata out of the image
       jhead -purejpg "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}"
 
       # compress the image
-      convert "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}" -quality "${COMPRESSION_FACTOR}%" "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME_COMPRESSED}"
+      convert "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}" -quality "${COMPRESSION_FACTOR}%" "${PHOTO_OUTPUT_DIR}/${OUTPUT_FILE_NAME}"
 
-      echo "Finished Image "${OUTPUT_FILE_NAME_COMPRESSED}
+      echo "Finished Image "${OUTPUT_FILE_NAME}
     else
       echo "Image ${FILE_NAME} already converted"
     fi
